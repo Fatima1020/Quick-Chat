f2ab549c447f3512a945cab0c435ec3fff2e5845
Fatima1020 <preppy4mimi@gmail.com>
2026-05-04 02:26:18 -0400

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,7 +6,7 @@ import { io } from 'socket.io-client';
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const socket = io('http://localhost:5000');
+const socket = io('https://quick-chat-backend-production.up.railway.app');
 
 function Home(){
    const { selectedChat, user } = useSelector(state => state.userReducer);
