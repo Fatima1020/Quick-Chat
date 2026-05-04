aad40650cf61de49e9f2b561817147f1d084fe85
Fatima1020 <preppy4mimi@gmail.com>
2026-05-04 02:25:35 -0400

    Update index.js

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:5000"
+export const url = "https://quick-chat-backend-production.up.railway.app"
 
 export const axiosInstance = axios.create({
     headers: {
