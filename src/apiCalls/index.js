d4cbdf79e5c90c388d9c71769c3b75f56cfecc69
Fatima1020 <preppy4mimi@gmail.com>
2026-05-04 04:23:56 -0400

    Add trailing slash to API base URL

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-export const url = "https://quick-chat-backend-production.up.railway.app"
+export const url = "https://quick-chat-backend-production.up.railway.app/"
 
 export const axiosInstance = axios.create({
+    baseURL: url,
     headers: {
         "Content-Type": "application/json",
         authorization: `Bearer ${localStorage.getItem('token')}`,
